fa63e1862a11b0d532e99455fc07bf68344821af
elie <elie>
2004-04-12 09:32:34 +0000

    branch version-3-3-x-branch joint back to main trunk

--- pysnmp/mapping/udp/cli/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-   SNMP framework for Python.
-
-   The pysnmp.mapping.udp.cli sub-package implements various command line
-   interfaces (CLI) used for transport objects initialization from command line.
-
-   Copyright 1999-2003 by Ilya Etingof <ilya@glas.net>. See LICENSE for details.
-"""
--- pysnmp/proto/cli/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-    SNMP framework for Python.
-
-    The pysnmp.proto.cli sub-package implements various command line
-    interface (CLI) for initializing various SNMP messages.
-
-   Copyright 1999-2003 by Ilya Etingof <ilya@glas.net>. See LICENSE for details.
-"""
