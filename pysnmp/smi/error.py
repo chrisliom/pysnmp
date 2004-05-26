47996c4d7beb367c0a9b965169f908e6bef9c71a
elie <elie>
2004-05-26 15:02:29 +0000

    more exceptions added (those comprise a protocol for SNMP protocol<->agent error reporting)

--- pysnmp/smi/error.py
+++ pysnmp/smi/error.py
@@ -6,5 +6,13 @@ class SmiImportError(SmiError): pass
 class BadArgumentError(SmiError): pass
 class NotImplementedError(SmiError): pass
 class MibError(SmiError): pass
-class NoSuchInstance(SmiError): pass
 
+class MibVariableError(SmiError): pass
+class NotInitializedError(MibVariableError): pass
+class NoSuchInstanceError(MibVariableError): pass
+class InconsistentValueError(MibVariableError): pass
+class WrongValueError(MibVariableError): pass
+class NoAccessError(MibVariableError): pass
+# Row management
+class RowCreationWanted(MibVariableError): pass
+class RowDestructionWanted(MibVariableError): pass
