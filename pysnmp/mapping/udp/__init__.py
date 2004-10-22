c8c1c21f49d9c9bee212870370e5e8e5f26ad9e3
elie <elie>
2004-10-22 15:02:27 +0000

    legacy transport implementation dropped

--- pysnmp/mapping/udp/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-   SNMP framework for Python.
-
-   The pysnmp.mapping.udp sub-package implements SNMP over UDP over IPv4
-   transport mapping.
-
-   Copyright 1999-2004 by Ilya Etingof <ilya@glas.net>. See LICENSE for details.
-"""
