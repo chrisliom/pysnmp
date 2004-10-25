43a3cc4b003c9ffa8c94f8ff161f3925dc6965a6
elie <elie>
2004-10-25 06:47:50 +0000

    converted to mix-in pattern

--- pysnmp/proto/rfc3411.py
+++ pysnmp/proto/rfc3411.py
@@ -1,86 +1,46 @@
-"""
-   An Architecture for Describing SNMP Management Frameworks (RFC3411).
+from pysnmp.proto import rfc1157, rfc1905
 
-   Written by Ilya Etingof <ilya@glas.net>, 2002.
-"""
-# Module public names
-__all__ = [ 'GetRequestPdu', 'GetNextRequestPdu', 'ResponsePdu', \
-            'SetRequestPdu', 'GetBulkRequestPdu', 'InformRequestPdu', \
-            'SnmpV2TrapPdu', 'ReportPdu', 'ReadClass', 'WriteClass',
-            'ResponseClass', 'NotificationClass', 'InternalClass',
-            'ConfirmedClass', 'UnconfirmedClass' ]
-
-from pysnmp.proto import rfc1905
+__all__ = [
+    'ReadClassMixIn', 'WriteClassMixIn', 'ResponseClassMixIn',
+    'NotificationClassMixIn', 'InternalClassMixIn', 
+    'ConfirmedClassMixIn', 'UnconfirmedClassMixIn'
+    ]
 
 # Functional PDU classification
 
-class ReadClass:
-    """Protocol operations that retrieve management information
-    """
-    pass
-
-class WriteClass:
-    """Protocol operations which attempt to modify management information
-    """
-    pass
-
-class ResponseClass:
-    """Protocol operations which are sent in response to a previous request
-    """
-    pass
-
-class NotificationClass:
-    """Protocol operations which send a notification to a notification
-       receiver application.
-    """
-    pass
-
-class InternalClass:
-    """Protocol operations which are exchanged internally between
-       SNMP engines.
-    """
-    pass
+class ReadClassMixIn: pass
+class WriteClassMixIn: pass
+class ResponseClassMixIn: pass
+class NotificationClassMixIn: pass
+class InternalClassMixIn: pass
 
 # PDU classification based on whether a response is expected
 
-class ConfirmedClass:
-    """Protocol operations which cause the receiving SNMP engine
-       to send back a response.
-    """
-    pass
+class ConfirmedClassMixIn: pass
+class UnconfirmedClassMixIn: pass
 
-class UnconfirmedClass:
-    """Protocol operations which are not acknowledged
-    """
-    pass
+# Classify various PDU types    
 
-# Classify RFC1905 PDU types
+__mixInMatrix = (
+    # RFC1157 types
+    (rfc1157.GetRequestPdu, (ReadClassMixIn, ConfirmedClassMixIn)),
+    (rfc1157.GetNextRequestPdu, (ReadClassMixIn, ConfirmedClassMixIn)),
+    (rfc1157.SetRequestPdu, (WriteClassMixIn, ConfirmedClassMixIn)),
+    (rfc1157.GetResponsePdu, (ResponseClassMixIn, UnconfirmedClassMixIn)),
+    (rfc1157.GetResponsePdu, ( ResponseClassMixIn, UnconfirmedClassMixIn)),
+    (rfc1157.TrapPdu, (NotificationClassMixIn, UnconfirmedClassMixIn)),
+    # RFC1905 types
+    (rfc1905.GetRequestPdu, (ReadClassMixIn, ConfirmedClassMixIn)),
+    (rfc1905.GetNextRequestPdu, (ReadClassMixIn, ConfirmedClassMixIn)),
+    (rfc1905.GetBulkRequestPdu, (ReadClassMixIn, ConfirmedClassMixIn)),
+    (rfc1905.SetRequestPdu, (WriteClassMixIn, ConfirmedClassMixIn)),
+    (rfc1905.ResponsePdu, (ResponseClassMixIn, UnconfirmedClassMixIn)),
+    (rfc1905.ReportPdu, (ResponseClassMixIn, UnconfirmedClassMixIn,
+                         InternalClassMixIn)),
+    (rfc1905.SnmpV2TrapPdu, (NotificationClassMixIn, UnconfirmedClassMixIn))
+    )
 
-class GetRequestPdu(rfc1905.GetRequestPdu, ReadClass, ConfirmedClass):
-    pass
-
-class GetNextRequestPdu(rfc1905.GetNextRequestPdu, ReadClass, ConfirmedClass):
-    pass
-
-class GetBulkRequestPdu(rfc1905.GetBulkRequestPdu, ReadClass, ConfirmedClass):
-    pass
-
-class SetRequestPdu(rfc1905.GetRequestPdu, WriteClass, ConfirmedClass):
-    pass
-
-class ResponsePdu(rfc1905.ResponsePdu, ResponseClass, UnconfirmedClass):
-    pass
-
-class ReportPdu(rfc1905.ReportPdu, ResponseClass, UnconfirmedClass):
-    pass
-
-class SnmpV2TrapPdu(rfc1905.SnmpV2TrapPdu, NotificationClass,
-                    UnconfirmedClass):
-    pass
-
-class InformRequestPdu(rfc1905.InformRequestPdu, NotificationClass,
-                       ConfirmedClass):
-    pass
-
-class ReportPdu(rfc1905.ReportPdu, InternalClass, UnconfirmedClass):
-    pass
+for baseClass, mixIns in __mixInMatrix:
+    for mixIn in mixIns:
+        if mixIn not in baseClass.__bases__:
+            baseClass.__bases__ = (mixIn, ) + baseClass.__bases__
