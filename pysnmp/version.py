3d2a1c7e47f4a04037dcd2a44d0156c17e75ebe0
elie <elie>
2004-10-25 06:25:33 +0000

    bugfix to version verification code

--- pysnmp/version.py
+++ pysnmp/version.py
@@ -3,17 +3,19 @@
 import error
 
 __versionMajor = 3
-__versionMinor = 4
-__patchLevel = 3
+__versionMinor = 5
+__patchLevel = 1
 
 def getVersion(): return (__versionMajor, __versionMinor, __patchLevel)
 def verifyVersionRequirement(versionMajor, versionMinor, patchLevel=0):
-    if __versionMajor < versionMajor or __versionMinor < versionMinor:
+    if __versionMajor < versionMajor or \
+       __versionMajor == versionMajor and __versionMinor < versionMinor:
         raise error.PySnmpVersionError(
             'PySNMP version %s.%s or higher required' %
             (versionMajor, versionMinor)
         )
-    if patchLevel > 0 and __patchLevel < patchLevel:
+    if __versionMajor == versionMajor and __versionMinor == versionMinor and \
+           patchLevel > 0 and __patchLevel < patchLevel:
         raise error.PySnmpVersionError(
             'PySNMP version %s.%s (patch level %s) or higher required' %
             (versionMajor, versionMinor, patchLevel)
