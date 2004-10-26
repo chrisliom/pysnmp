18213286d9c65533fd687288eacc0d029fd536dc
elie <elie>
2004-10-26 13:07:39 +0000

    rfc1155->univ

--- pysnmp/proto/api/alpha/v1.py
+++ pysnmp/proto/api/alpha/v1.py
@@ -1,10 +1,12 @@
 from pysnmp.proto.rfc1155 import *
 from pysnmp.proto.rfc1157 import *
-from pysnmp.proto.api.alpha import rfc1155, rfc1157, pdutypes
+from pysnmp.proto.api.alpha import univ, rfc1157, pdutypes
 
-pduTypes = { pdutypes.GetRequestPduType(): GetRequestPdu,
-             pdutypes.GetNextRequestPduType(): GetNextRequestPdu,
-             pdutypes.SetRequestPduType(): SetRequestPdu,
-             pdutypes.GetResponsePduType(): GetResponsePdu,
-             pdutypes.TrapPduType(): TrapPdu }
+pduTypes = {
+    pdutypes.GetRequestPduType(): GetRequestPdu,
+    pdutypes.GetNextRequestPduType(): GetNextRequestPdu,
+    pdutypes.SetRequestPduType(): SetRequestPdu,
+    pdutypes.GetResponsePduType(): GetResponsePdu,
+    pdutypes.TrapPduType(): TrapPdu
+    }
 
--- pysnmp/proto/api/alpha/v2c.py
+++ pysnmp/proto/api/alpha/v2c.py
@@ -1,16 +1,18 @@
 from pysnmp.proto.rfc1902 import *
 from pysnmp.proto.rfc1905 import *
-from pysnmp.proto.api.alpha import rfc1905, pdutypes
+from pysnmp.proto.api.alpha import univ, rfc1905, pdutypes
 
 # Proto versions compatibility alias
 GetResponsePdu = ResponsePdu
 TrapPdu = SnmpV2TrapPdu
 
-pduTypes = { pdutypes.GetRequestPduType(): GetRequestPdu,
-             pdutypes.GetNextRequestPduType(): GetNextRequestPdu,
-             pdutypes.SetRequestPduType(): SetRequestPdu,
-             pdutypes.GetResponsePduType(): ResponsePdu,
-             pdutypes.TrapPduType(): SnmpV2TrapPdu,
-             pdutypes.GetBulkRequestPduType(): GetBulkRequestPdu,             
-             pdutypes.InformRequestPduType(): InformRequestPdu,
-             pdutypes.ReportPduType(): ReportPdu }
+pduTypes = {
+    pdutypes.GetRequestPduType(): GetRequestPdu,
+    pdutypes.GetNextRequestPduType(): GetNextRequestPdu,
+    pdutypes.SetRequestPduType(): SetRequestPdu,
+    pdutypes.GetResponsePduType(): ResponsePdu,
+    pdutypes.TrapPduType(): SnmpV2TrapPdu,
+    pdutypes.GetBulkRequestPduType(): GetBulkRequestPdu,             
+    pdutypes.InformRequestPduType(): InformRequestPdu,
+    pdutypes.ReportPduType(): ReportPdu
+    }
