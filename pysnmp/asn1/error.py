b9c03ef239494b88c3cd4d3345ed8b95240c42aa
elie <elie>
2004-11-11 16:31:37 +0000

    NotImplementedError removed

--- pysnmp/asn1/error.py
+++ pysnmp/asn1/error.py
@@ -4,5 +4,4 @@ from pysnmp import error
 class Asn1Error(error.PySnmpError): pass
 class BadArgumentError(Asn1Error): pass
 class ValueConstraintError(Asn1Error): pass
-class NotImplementedError(Asn1Error): pass
 
