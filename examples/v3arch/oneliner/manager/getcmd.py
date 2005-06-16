a99c2a1274307a8274e8b31acb04821850d5733c
elie <elie>
2005-06-16 07:08:17 +0000

    adjusted to mibvar-based input variable syntax

--- examples/v3arch/oneliner/manager/getcmd.py
+++ examples/v3arch/oneliner/manager/getcmd.py
@@ -3,7 +3,10 @@ from pysnmp.entity.rfc3413.oneliner import cmdgen
 errorIndication, errorStatus, errorIndex, varBinds = cmdgen.CmdGen().snmpGet(
     cmdgen.UsmUserData('test-user', 'authkey1', 'privkey1'),
     cmdgen.UdpTransportTarget(('localhost', 161)),
-    'SNMPv2-MIB::sysDescr.0'
+    (('sysDescr', 'SNMPv2-MIB'), 0),
+    (('sysDescr',), 0),
+    ('iso','org','dod','internet','mgmt','mib-2','system','sysDescr',0),
+    (1,3,6,1,2,1,1,1,0)
     )
 
 if errorIndication:
@@ -12,4 +15,5 @@ else:
     if errorStatus:
         print '%s at %s\n' % (errorStatus, varBinds[errorIndex-1])
     else:
-        print varBinds
+        for varBind in varBinds:
+            print '%s = %s' % varBind
