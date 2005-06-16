787a534feb53bdc72967c762825ece5caa72fe2a
elie <elie>
2005-06-16 07:33:28 +0000

    initial revision

---
+++ examples/v3arch/oneliner/manager/bulkcmd.py
@@ -0,0 +1,19 @@
+from pysnmp.entity.rfc3413.oneliner import cmdgen
+
+errorIndication, errorStatus, errorIndex, \
+                 varBinds, varBindTable = cmdgen.CmdGen().snmpBulkWalk(
+    cmdgen.UsmUserData('test-user', 'authkey1', 'privkey1'),
+    cmdgen.UdpTransportTarget(('localhost', 161)),
+    0, 25,
+    (('system',),)
+    )
+
+if errorIndication:
+    print errorIndication
+else:
+    if errorStatus:
+        print '%s at %s\n' % (errorStatus, varBinds[errorIndex-1])
+    else:
+        for varBindTableRow in varBindTable:
+            for varBind in varBindTableRow:
+                print '%s = %s' % varBind
