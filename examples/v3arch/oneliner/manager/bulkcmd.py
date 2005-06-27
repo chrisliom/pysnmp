ddf32fd9556c648b69a3fa73df1b1527303d6c67
elie <elie>
2005-06-27 08:06:39 +0000

    remove args requiring MIB resolution from this example

--- examples/v3arch/oneliner/manager/bulkcmd.py
+++ examples/v3arch/oneliner/manager/bulkcmd.py
@@ -2,10 +2,13 @@ from pysnmp.entity.rfc3413.oneliner import cmdgen
 
 errorIndication, errorStatus, errorIndex, \
                  varBinds, varBindTable = cmdgen.CmdGen().bulkCmd(
+    # SNMP v2
+#    cmdgen.CommunityData('test-agent', 'public'),
+    # SNMP v3
     cmdgen.UsmUserData('test-user', 'authkey1', 'privkey1'),
     cmdgen.UdpTransportTarget(('localhost', 161)),
     0, 25,
-    (('system',),)
+    (1,3,6,1,2,1,1)
     )
 
 if errorIndication:
