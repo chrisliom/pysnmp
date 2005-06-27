ac9b816ec0cb3d0778eaed7dd063ddb28efb8745
elie <elie>
2005-06-27 12:42:07 +0000

    minor syntax fixes followed pychecker report

--- pysnmp/v4/entity/rfc3413/error.py
+++ pysnmp/v4/entity/rfc3413/error.py
@@ -2,6 +2,7 @@ from pysnmp.error import PySnmpError
 
 class ApplicationReturn(PySnmpError):
     def __init__(self, **kwargs):
+        PySnmpError.__init__(self)
         self.__errorIndication = kwargs
     def __str__(self): return str(self.__errorIndication)
     def __getitem__(self, key): return self.__errorIndication[key]
