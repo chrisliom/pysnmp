1a0fa71072e8399947a5a47dd1988917aac67536
elie <elie>
2005-06-28 20:25:11 +0000

    comments cleaned up

--- examples/v3arch/oneliner/manager/withmib/nextcmd.py
+++ examples/v3arch/oneliner/manager/withmib/nextcmd.py
@@ -7,16 +7,16 @@ cmdGen = cmdgen.CmdGen()
 errorIndication, errorStatus, errorIndex, \
                  varBinds, varBindTable = cmdGen.nextCmd(
     # SNMP v1
-    cmdgen.CommunityData('test-agent', 'public', 0),
+#    cmdgen.CommunityData('test-agent', 'public', 0),
     # SNMP v2
 #    cmdgen.CommunityData('test-agent', 'public'),
     # SNMP v3
-#    cmdgen.UsmUserData('test-user', 'authkey1', 'privkey1'),
+    cmdgen.UsmUserData('test-user', 'authkey1', 'privkey1'),
     cmdgen.UdpTransportTarget(('localhost', 161)),
 #    (('TCP-MIB', ''),),
     (('SNMPv2-MIB', ''),),
-#    (('IF-MIB', ''),),
 #    (('IP-MIB', ''),),
+#    (('IF-MIB', ''),),
 #    (('', 'interfaces'),),
 #    (1,3,6,1,2,1)
 #    (('','system'),)
