216e0f24ced0956c45e5cf7fd34a5b07eef9defc
elie <elie>
2006-12-13 20:23:33 +0000

    API version selection now picks the most recent version, __import__() call fixed (SF tracker #1612990)

--- pysnmp/__init__.py
+++ pysnmp/__init__.py
@@ -5,7 +5,7 @@ import string
 
 def switchApiVersion(subPkg):
     pkg = os.path.split(__path__[0])[-1]
-    newMod = __import__(subPkg, globals(), locals(), pkg)
+    newMod = __import__(subPkg, globals(), locals())
     realPkg = '_real_' + pkg
     if sys.modules.has_key(realPkg):
         sys.modules[pkg] = sys.modules[realPkg]
@@ -17,12 +17,11 @@ def __isSubPackage(subDir):
            and len(subDir) == 2:
         return 1
 
-subDirs = filter(__isSubPackage, os.listdir(__path__[0]))
-subDirs.sort(); subDirs.reverse()
-
 if os.environ.has_key('PYSNMP_API_VERSION'):
     v = os.environ['PYSNMP_API_VERSION']
     if v:
         switchApiVersion(v)   # do not load any API
 else:
+    subDirs = filter(__isSubPackage, os.listdir(__path__[0]))
+    subDirs.sort();
     switchApiVersion(subDirs[-1])  # take the most recent version
